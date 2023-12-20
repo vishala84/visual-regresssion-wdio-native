30b76eec1ec230c5d77bfd7a06931ebf2e73f762
vishala84 <67219707+vishala84@users.noreply.github.com>
2023-12-20 13:34:24 +0530

    Fix issues

--- src/compareimage.feature
+++ src/compareimage.feature
@@ -1,4 +1,5 @@
 Feature: Compare Image
 
   Scenario: As a user, I login to Capsyl
-    When User gets screenshot of thumbnail
+    When User launches app
+    Then User gets screenshot of thumbnail
--- src/step-definitions/compareImage-steps.ts
+++ src/step-definitions/compareImage-steps.ts
@@ -1,7 +1,11 @@
 import {When} from "@wdio/cucumber-framework";
 import {driver} from "@wdio/globals";
 
-When(/^User gets screenshot of thumbnail$/, async function () {
+Then(/^User gets screenshot of thumbnail$/, async function () {
     let element = await driver.findElement('id','elementIdHere');
     await driver.compareElement(<WebdriverIO.Element>element, 'firstElement', {});
+});
+
+When(/^User launches app$/,async function(){
+    await driver.launchApp()
 });
